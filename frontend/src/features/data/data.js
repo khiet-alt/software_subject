9e36559d1f6aae19141c5140ab08f18b70ecd71c
thanh khiet <cs.thanhkhiet@gmail.com>
2022-06-14 23:09:16 +0700

    try to fix r.map error when deploy to heroku

--- frontend/src/features/data/data.js
+++ frontend/src/features/data/data.js
@@ -1,10 +1,9 @@
 const API = {
     ORIGIN: 'http://localhost:5000/',
-    // API_USER_LOCAL: 'http://localhost:5000/api/users/'
 }
 
-if (process.env.NODE_ENV === 'production'){
-    API.ORIGIN = ''
-}
+// if (process.env.NODE_ENV === 'production'){
+//     API.ORIGIN = ''
+// }
 
 exports.API = API
